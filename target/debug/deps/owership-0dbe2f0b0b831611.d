f5d31011868a6b3485b57e347eded38b41dc885b
shuliaka95 <58136995+shuliaka95@users.noreply.github.com>
2025-04-01 10:32:29 +0300

    feat: инициализация проекта с Cargo.toml

---
+++ target/debug/deps/owership-0dbe2f0b0b831611.d
@@ -0,0 +1,5 @@
+C:\Users\ASUS\owership\target\debug\deps\libowership-0dbe2f0b0b831611.rmeta: src\main.rs
+
+C:\Users\ASUS\owership\target\debug\deps\owership-0dbe2f0b0b831611.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/owership-15431739f1c90555.d
@@ -0,0 +1,5 @@
+C:\Users\ASUS\owership\target\debug\deps\libowership-15431739f1c90555.rmeta: src\main.rs
+
+C:\Users\ASUS\owership\target\debug\deps\owership-15431739f1c90555.d: src\main.rs
+
+src\main.rs:
